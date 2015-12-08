71e4e92e6e3af398736f033c30f55767c1c36299
chgsit00 <C.Gschrey@gmx.net>
2015-12-08 16:22:20 +0100

    Exception gelöscht

--- app/src/main/java/daimler/geocacheme/GPSNotActiveException.java
+++
@@ -1,9 +0,0 @@
-package daimler.geocacheme;
-
-/**
- * Created by CGsch on 04.11.2015.
- */
-public class GPSNotActiveException extends Exception
-{
-    public String Message = "GPS is currently not available";
-}
